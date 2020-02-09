cf235e3827482d0f7892375fc9ddbfd3b3df493f
Luca Morricone <morricone.luca@outlook.com>
2020-02-10 00:30:44 +0100

    Add get questions actions

---
+++ src/actions/questions.js
@@ -0,0 +1,17 @@
+import {_getQuestions} from '../api/_DATA';
+
+export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS';
+
+export function receiveQuestions(questions) {
+  return {
+    type: RECEIVE_QUESTIONS,
+    questions
+  };
+}
+
+export function handleGetQuestions() {
+  return async dispatch => {
+    const questions = await _getQuestions();
+    dispatch(receiveQuestions(questions));
+  };
+}
