43f6a0bcc6d1dc0f43d3ca17068bb1a783d21501
Luca Morricone <morricone.luca@outlook.com>
2020-02-09 19:34:26 +0100

    Add get users actions

---
+++ src/actions/users.js
@@ -0,0 +1,17 @@
+import {_getUsers} from '../api/_DATA';
+
+export const RECEIVE_USERS = 'RECEIVE_USERS';
+
+export function receiveUsers(users) {
+  return {
+    type: RECEIVE_USERS,
+    users
+  };
+}
+
+export function handleGetUsers() {
+  return async dispatch => {
+    const users = await _getUsers();
+    dispatch(receiveUsers(users));
+  };
+}
