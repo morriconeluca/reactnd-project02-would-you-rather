63e4f78c511353d04de87b1cc676029f7a8bf562
Luca Morricone <morricone.luca@outlook.com>
2020-02-11 14:37:15 +0100

    Change avatar images and data

--- src/api/_DATA.js
+++ src/api/_DATA.js
@@ -1,8 +1,8 @@
 let users = {
-  birba: {
-    id: 'birba',
-    name: 'Birba',
-    avatarURL: '/avatar/Birba.png',
+  geneviemcpeak: {
+    id: 'geneviemcpeak',
+    name: 'Genevie Mcpeak',
+    avatarURL: '/avatar/geneviemcpeak.png',
     answers: {
       '8xf0y6ziyjabvozdd253nd': 'optionOne',
       '6ni6ok3ym7mf1p33lnez': 'optionTwo',
@@ -11,20 +11,20 @@ let users = {
     },
     questions: ['8xf0y6ziyjabvozdd253nd', 'am8ehyc8byjqgar0jgpub9']
   },
-  bigio: {
-    id: 'bigio',
-    name: 'Bigio',
-    avatarURL: '/avatar/Bigio.png',
+  wardkildow: {
+    id: 'wardkildow',
+    name: 'Ward Kildow',
+    avatarURL: '/avatar/wardkildow.png',
     answers: {
       'vthrdm985a262al8qx3do': 'optionOne',
       'xj352vofupe1dqz9emx13r': 'optionTwo',
     },
     questions: ['loxhs1bqm25b708cmbf3g', 'vthrdm985a262al8qx3do']
   },
-  buricchio: {
-    id: 'buricchio',
-    name: 'Buricchio',
-    avatarURL: '/avatar/Buricchio.png',
+  rayfordstreet: {
+    id: 'rayfordstreet',
+    name: 'Rayford Street',
+    avatarURL: '/avatar/rayfordstreet.png',
     answers: {
       'xj352vofupe1dqz9emx13r': 'optionOne',
       'vthrdm985a262al8qx3do': 'optionTwo',
@@ -32,38 +32,66 @@ let users = {
     },
     questions: ['6ni6ok3ym7mf1p33lnez', 'xj352vofupe1dqz9emx13r']
   },
-  cristina: {
-    id: 'cristina',
-    name: 'Cristina',
-    avatarURL: '/avatar/Cristina.png',
+  louschwartz: {
+    id: 'louschwartz',
+    name: 'Lou Schwartz',
+    avatarURL: '/avatar/louschwartz.png',
     answers: {},
     questions: []
   },
-  luca: {
-    id: 'luca',
-    name: 'Luca',
-    avatarURL: '/avatar/Luca.png',
+  carlineaden: {
+    id: 'carlineaden',
+    name: 'Carline Aden',
+    avatarURL: '/avatar/carlineaden.png',
     answers: {},
     questions: []
   },
-  nero: {
-    id: 'nero',
-    name: 'Nero',
-    avatarURL: '/avatar/Nero.png',
+  johnnyschlicher: {
+    id: 'johnnyschlicher',
+    name: 'Johnny Schlicher',
+    avatarURL: '/avatar/johnnyschlicher.png',
     answers: {},
     questions: []
   },
-  peppuccio: {
-    id: 'peppuccio',
-    name: 'Peppuccio',
-    avatarURL: '/avatar/Peppuccio.png',
+  ammiekittel: {
+    id: 'ammiekittel',
+    name: 'Ammie Kittel',
+    avatarURL: '/avatar/ammiekittel.png',
     answers: {},
     questions: []
   },
-  salsiccia: {
-    id: 'salsiccia',
-    name: 'Salsiccia',
-    avatarURL: '/avatar/Salsiccia.png',
+  lucretialasky: {
+    id: 'lucretialasky',
+    name: 'Lucretia Lasky',
+    avatarURL: '/avatar/lucretialasky.png',
+    answers: {},
+    questions: []
+  },
+  brandensilber: {
+    id: 'brandensilber',
+    name: 'Branden Silber',
+    avatarURL: '/avatar/brandensilber.png',
+    answers: {},
+    questions: []
+  },
+  latanyahavener: {
+    id: 'latanyahavener',
+    name: 'Latanya Havener',
+    avatarURL: '/avatar/latanyahavener.png',
+    answers: {},
+    questions: []
+  },
+  columbuscallas: {
+    id: 'columbuscallas',
+    name: 'Columbus Callas',
+    avatarURL: '/avatar/columbuscallas.png',
+    answers: {},
+    questions: []
+  },
+  raneecrumpton: {
+    id: 'raneecrumpton',
+    name: 'Ranee Crumpton',
+    avatarURL: '/avatar/raneecrumpton.png',
     answers: {},
     questions: []
   }
@@ -72,10 +100,10 @@ let users = {
 let questions = {
   '8xf0y6ziyjabvozdd253nd': {
     id: '8xf0y6ziyjabvozdd253nd',
-    author: 'birba',
+    author: 'geneviemcpeak',
     timestamp: 1467166872634,
     optionOne: {
-      votes: ['birba'],
+      votes: ['geneviemcpeak'],
       text: 'have horrible short term memory'
     },
     optionTwo: {
@@ -85,66 +113,66 @@ let questions = {
   },
   '6ni6ok3ym7mf1p33lnez': {
     id: '6ni6ok3ym7mf1p33lnez',
-    author: 'buricchio',
+    author: 'rayfordstreet',
     timestamp: 1468479767190,
     optionOne: {
       votes: [],
       text: 'become a superhero'
     },
     optionTwo: {
-      votes: ['buricchio', 'birba'],
+      votes: ['rayfordstreet', 'geneviemcpeak'],
       text: 'become a supervillain'
     }
   },
   'am8ehyc8byjqgar0jgpub9': {
     id: 'am8ehyc8byjqgar0jgpub9',
-    author: 'birba',
+    author: 'geneviemcpeak',
     timestamp: 1488579767190,
     optionOne: {
       votes: [],
       text: 'be telekinetic'
     },
     optionTwo: {
-      votes: ['birba'],
+      votes: ['geneviemcpeak'],
       text: 'be telepathic'
     }
   },
   'loxhs1bqm25b708cmbf3g': {
     id: 'loxhs1bqm25b708cmbf3g',
-    author: 'bigio',
+    author: 'wardkildow',
     timestamp: 1482579767190,
     optionOne: {
       votes: [],
       text: 'be a front-end developer',
     },
     optionTwo: {
-      votes: ['birba'],
+      votes: ['geneviemcpeak'],
       text: 'be a back-end developer'
     }
   },
   'vthrdm985a262al8qx3do': {
     id: 'vthrdm985a262al8qx3do',
-    author: 'bigio',
+    author: 'wardkildow',
     timestamp: 1489579767190,
     optionOne: {
-      votes: ['bigio'],
+      votes: ['wardkildow'],
       text: 'find $50 yourself'
     },
     optionTwo: {
-      votes: ['buricchio'],
+      votes: ['rayfordstreet'],
       text: 'have your best friend find $500'
     }
   },
   'xj352vofupe1dqz9emx13r': {
     id: 'xj352vofupe1dqz9emx13r',
-    author: 'buricchio',
+    author: 'rayfordstreet',
     timestamp: 1493579767190,
     optionOne: {
-      votes: ['buricchio'],
+      votes: ['rayfordstreet'],
       text: 'write JavaScript'
     },
     optionTwo: {
-      votes: ['bigio'],
+      votes: ['wardkildow'],
       text: 'write Swift'
     }
   }
